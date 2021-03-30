e37a7edb39329752df847aabd41f78db0c376733
Fan Yang <fan.yang.oxford@gmail.com>
2021-03-30 16:22:33 +0000

    show folder size

--- clean_files.sh
+++ clean_files.sh
@@ -5,18 +5,23 @@
 # 0 20 * * * sh /home/ubuntu/src/misctools/clean_files.sh > /tmp/clean_out.txt
 
 date
+du -kh /data/inputs/local --max-depth=0
+echo "=============cleaning /data/inputs/local"
+find /data/inputs/local -type f -mtime +5 -exec rm -f {} ';'
+find /data/inputs/local -type d -mtime +5 -exec rm -r {} ';'
+du -kh /data/inputs/local --max-depth=0
 
-echo "cleaning /data/inputs/local"
-find /data/inputs/local -type f -mtime +7 -exec rm -f {} ';'
-find /data/inputs/local -type d -mtime +7 -exec rm -r {} ';'
-
-echo "cleaning /data/inputs/users"
+du -kh /data/inputs/users --max-depth=0
+echo "=============cleaning /data/inputs/users"
 find /data/inputs/users -type f -mtime +14 -exec rm -f {} ';'
 find /data/inputs/users -type d -mtime +14 -exec rm -r {} ';'
+du -kh /data/inputs/users --max-depth=0
 
-echo "cleaning /data/inputs/ena"
+du -kh /data/inputs/ena --max-depth=0
+echo "=============cleaning /data/inputs/ena"
 find /data/inputs/ena -type f -mtime +14 -exec rm -f {} ';'
 find /data/inputs/ena -type d -mtime +14 -exec rm -r {} ';'
+du -kh /data/inputs/ena --max-depth=0
 
 echo "Completed cleaning"
 
